3502f53bae303e4733ef585cf9222a4660d407a6
Christoph Olszowka <christoph@olszowka.de>
2020-05-30 22:45:03 +0200

    Basic bundler plugin setup

---
+++ plugins.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+#
+# Bundler plugin integration
+#
+# For more information about how to setup bundler plugins see:
+# https://bundler.io/guides/bundler_plugins.html
+#
+
+require "bundler-toolbox"
+require "bundler/toolbox/plugins/bundler"
